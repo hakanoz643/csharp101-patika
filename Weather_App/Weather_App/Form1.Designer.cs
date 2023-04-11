fc51f769ead1618e2ae351ea20c3630fce61ab96
hakanoz643 <121900300+hakanoz643@users.noreply.github.com>
2023-04-11 23:48:05 +0300

    Weather App
    Added

---
+++ Weather_App/Weather_App/Form1.Designer.cs
@@ -0,0 +1,339 @@
+﻿namespace Weather_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Label label2;
+            this.exitbutton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cityinput = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.durumtext = new System.Windows.Forms.Label();
+            this.detaytext = new System.Windows.Forms.Label();
+            this.ruzgarhizitext = new System.Windows.Forms.Label();
+            this.basinctext = new System.Windows.Forms.Label();
+            this.sicakliktext = new System.Windows.Forms.Label();
+            this.durumimage = new System.Windows.Forms.PictureBox();
+            this.citytext = new System.Windows.Forms.Label();
+            this.saat = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.hissedilentext = new System.Windows.Forms.Label();
+            label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.durumimage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = System.Drawing.Color.Transparent;
+            label2.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            label2.ForeColor = System.Drawing.Color.NavajoWhite;
+            label2.Location = new System.Drawing.Point(109, 516);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(104, 31);
+            label2.TabIndex = 8;
+            label2.Text = "Sıcaklık:";
+            // 
+            // exitbutton
+            // 
+            this.exitbutton.BackColor = System.Drawing.Color.Transparent;
+            this.exitbutton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.exitbutton.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.exitbutton.ForeColor = System.Drawing.Color.Transparent;
+            this.exitbutton.Location = new System.Drawing.Point(1047, 652);
+            this.exitbutton.Name = "exitbutton";
+            this.exitbutton.Size = new System.Drawing.Size(98, 29);
+            this.exitbutton.TabIndex = 0;
+            this.exitbutton.Text = "Exit";
+            this.exitbutton.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.exitbutton.UseVisualStyleBackColor = false;
+            this.exitbutton.Click += new System.EventHandler(this.exitbutton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.NavajoWhite;
+            this.label1.Location = new System.Drawing.Point(573, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 37);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Şehir :";
+            // 
+            // cityinput
+            // 
+            this.cityinput.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.cityinput.ForeColor = System.Drawing.Color.NavajoWhite;
+            this.cityinput.Location = new System.Drawing.Point(677, 66);
+            this.cityinput.Name = "cityinput";
+            this.cityinput.Size = new System.Drawing.Size(217, 34);
+            this.cityinput.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.Color.NavajoWhite;
+            this.button1.Location = new System.Drawing.Point(913, 64);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 39);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Ara";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.NavajoWhite;
+            this.label3.Location = new System.Drawing.Point(109, 417);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 31);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Durum:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.Color.NavajoWhite;
+            this.label4.Location = new System.Drawing.Point(109, 471);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 31);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Detay:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.ForeColor = System.Drawing.Color.NavajoWhite;
+            this.label5.Location = new System.Drawing.Point(605, 471);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(144, 31);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Rüzgar Hızı:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label7.ForeColor = System.Drawing.Color.NavajoWhite;
+            this.label7.Location = new System.Drawing.Point(605, 516);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(89, 31);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Basınç:";
+            // 
+            // durumtext
+            // 
+            this.durumtext.AutoSize = true;
+            this.durumtext.BackColor = System.Drawing.Color.Transparent;
+            this.durumtext.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.durumtext.ForeColor = System.Drawing.Color.NavajoWhite;
+            this.durumtext.Location = new System.Drawing.Point(210, 417);
+            this.durumtext.Name = "durumtext";
+            this.durumtext.Size = new System.Drawing.Size(56, 31);
+            this.durumtext.TabIndex = 11;
+            this.durumtext.Text = "null";
+            // 
+            // detaytext
+            // 
+            this.detaytext.AutoSize = true;
+            this.detaytext.BackColor = System.Drawing.Color.Transparent;
+            this.detaytext.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.detaytext.ForeColor = System.Drawing.Color.NavajoWhite;
+            this.detaytext.Location = new System.Drawing.Point(191, 471);
+            this.detaytext.Name = "detaytext";
+            this.detaytext.Size = new System.Drawing.Size(56, 31);
+            this.detaytext.TabIndex = 12;
+            this.detaytext.Text = "null";
+            // 
+            // ruzgarhizitext
+            // 
+            this.ruzgarhizitext.AutoSize = true;
+            this.ruzgarhizitext.BackColor = System.Drawing.Color.Transparent;
+            this.ruzgarhizitext.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.ruzgarhizitext.ForeColor = System.Drawing.Color.NavajoWhite;
+            this.ruzgarhizitext.Location = new System.Drawing.Point(755, 471);
+            this.ruzgarhizitext.Name = "ruzgarhizitext";
+            this.ruzgarhizitext.Size = new System.Drawing.Size(56, 31);
+            this.ruzgarhizitext.TabIndex = 14;
+            this.ruzgarhizitext.Text = "null";
+            // 
+            // basinctext
+            // 
+            this.basinctext.AutoSize = true;
+            this.basinctext.BackColor = System.Drawing.Color.Transparent;
+            this.basinctext.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.basinctext.ForeColor = System.Drawing.Color.NavajoWhite;
+            this.basinctext.Location = new System.Drawing.Point(713, 516);
+            this.basinctext.Name = "basinctext";
+            this.basinctext.Size = new System.Drawing.Size(56, 31);
+            this.basinctext.TabIndex = 15;
+            this.basinctext.Text = "null";
+            // 
+            // sicakliktext
+            // 
+            this.sicakliktext.AutoSize = true;
+            this.sicakliktext.BackColor = System.Drawing.Color.Transparent;
+            this.sicakliktext.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.sicakliktext.ForeColor = System.Drawing.Color.NavajoWhite;
+            this.sicakliktext.Location = new System.Drawing.Point(219, 516);
+            this.sicakliktext.Name = "sicakliktext";
+            this.sicakliktext.Size = new System.Drawing.Size(56, 31);
+            this.sicakliktext.TabIndex = 16;
+            this.sicakliktext.Text = "null";
+            // 
+            // durumimage
+            // 
+            this.durumimage.BackColor = System.Drawing.Color.Transparent;
+            this.durumimage.Location = new System.Drawing.Point(109, 329);
+            this.durumimage.Name = "durumimage";
+            this.durumimage.Size = new System.Drawing.Size(125, 61);
+            this.durumimage.TabIndex = 17;
+            this.durumimage.TabStop = false;
+            // 
+            // citytext
+            // 
+            this.citytext.AutoSize = true;
+            this.citytext.BackColor = System.Drawing.Color.Transparent;
+            this.citytext.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.citytext.ForeColor = System.Drawing.Color.NavajoWhite;
+            this.citytext.Location = new System.Drawing.Point(109, 71);
+            this.citytext.Name = "citytext";
+            this.citytext.Size = new System.Drawing.Size(84, 45);
+            this.citytext.TabIndex = 18;
+            this.citytext.Text = "null";
+            // 
+            // saat
+            // 
+            this.saat.AutoSize = true;
+            this.saat.BackColor = System.Drawing.Color.Transparent;
+            this.saat.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.saat.ForeColor = System.Drawing.Color.NavajoWhite;
+            this.saat.Location = new System.Drawing.Point(109, 123);
+            this.saat.Name = "saat";
+            this.saat.Size = new System.Drawing.Size(84, 45);
+            this.saat.TabIndex = 19;
+            this.saat.Text = "null";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label6.ForeColor = System.Drawing.Color.NavajoWhite;
+            this.label6.Location = new System.Drawing.Point(605, 417);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(221, 31);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "Hissedilen Sıcaklık:";
+            // 
+            // hissedilentext
+            // 
+            this.hissedilentext.AutoSize = true;
+            this.hissedilentext.BackColor = System.Drawing.Color.Transparent;
+            this.hissedilentext.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.hissedilentext.ForeColor = System.Drawing.Color.NavajoWhite;
+            this.hissedilentext.Location = new System.Drawing.Point(832, 417);
+            this.hissedilentext.Name = "hissedilentext";
+            this.hissedilentext.Size = new System.Drawing.Size(56, 31);
+            this.hissedilentext.TabIndex = 21;
+            this.hissedilentext.Text = "null";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Weather_App.Properties.Resources.kai_oberhauser_BKAaLmT0tIs_unsplash;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1147, 683);
+            this.Controls.Add(this.hissedilentext);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.saat);
+            this.Controls.Add(this.citytext);
+            this.Controls.Add(this.durumimage);
+            this.Controls.Add(this.sicakliktext);
+            this.Controls.Add(this.basinctext);
+            this.Controls.Add(this.ruzgarhizitext);
+            this.Controls.Add(this.detaytext);
+            this.Controls.Add(this.durumtext);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cityinput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.exitbutton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.durumimage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button exitbutton;
+        private Label label1;
+        private TextBox cityinput;
+        private Button button1;
+        private Label label3;
+        private Label label4;
+        private Label label2;
+        private Label label5;
+        private Label label7;
+        private Label durumtext;
+        private Label detaytext;
+        private Label ruzgarhizitext;
+        private Label basinctext;
+        private Label sicakliktext;
+        private PictureBox durumimage;
+        private Label citytext;
+        private Label saat;
+        private Label label6;
+        private Label hissedilentext;
+    }
+}
---
+++ Weather_App/Weather_App/Form1.cs
@@ -0,0 +1,54 @@
+using System.Net;
+using Newtonsoft.Json;
+using Weather_App.Models;
+
+namespace Weather_App
+{
+    public partial class Form1 : Form
+    {
+        public string apikey { get; set; } = "443310eda44a6ccac99dbfae70d44271";
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void exitbutton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GetHavaDurumu();
+        }
+        void GetHavaDurumu()
+        {
+            using (WebClient client = new WebClient())
+            {
+                var city = cityinput.Text;
+                string url = "https://api.openweathermap.org/data/2.5/weather?q="+ city + "&lang=tr&appid=" + apikey;
+                var data = client.DownloadString(url);
+
+                var jsondata = JsonConvert.DeserializeObject<Root>(data);
+                var iconadres  = "https://openweathermap.org/img/wn/" + jsondata.weather[0].icon +".png";
+
+                durumimage.ImageLocation= iconadres;
+                detaytext.Text = jsondata.weather[0].description.ToString();
+                durumtext.Text = jsondata.weather[0].main.ToString();
+
+                var sicaklik = ((jsondata.main.temp - 32) / Convert.ToDouble("1.8"));
+                sicakliktext.Text = Convert.ToInt32(sicaklik).ToString()+" 'C";
+                hissedilentext.Text = jsondata.main.humidity.ToString();
+
+                ruzgarhizitext.Text = jsondata.wind.speed.ToString();
+                basinctext.Text = jsondata.main.pressure.ToString();
+
+                citytext.Text = jsondata.name.ToString();
+                saat.Text = DateTime.Now.ToString("HH:mm");
+            }
+        }
+    }
+}
